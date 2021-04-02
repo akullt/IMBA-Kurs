b17b19950e28088888cd0074f72ea95c5b099177
akullt <lard85@bk.ru>
2021-04-02 17:44:36 +0300

    Добавил System.out.println 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-              System.out.println("Hello Hell\n");
+              System.out.println("Welcom to Hell");
               System.out.println("Оставь надежду всяк суда попавший\n");
+              System.out.print("Не обольщяйся глупыми мечтами\n");
+              System.out.println("УЧИСЬ ДАВАЙ!!!");
     }
 }
