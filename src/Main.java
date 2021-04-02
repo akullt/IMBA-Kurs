dd6ce63fd80fb18b76d18033f6057b6381189e39
akullt <lard85@bk.ru>
2021-04-02 17:53:59 +0300

    Добавил System.out.println 5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-              System.out.println("Welcom to Hell");
-              System.out.println("Оставь надежду всяк суда попавший\n");
-              System.out.print("Не обольщяйся глупыми мечтами\n");
+              System.out.println("Welcome to Hell");
+              System.out.println("Оставь надежду всяк суда попавший");
+              System.out.print("Не обольщайся глупыми мечтами Ты тот же приходяще-уходящий");
+              System.out.println(" И не надейся плыть под парусами ");
+              System.out.println("Ты не один, таких как ты навалом ");
+              System.out.println("Попользовался - уступи другим ");
+              System.out.println("Быть может за вот этим перевалом Ты станешь для кого-то дорогим.\n");
               System.out.println("УЧИСЬ ДАВАЙ!!!");
     }
 }
